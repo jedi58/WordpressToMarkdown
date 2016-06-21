cb44a4e3dbf3bc500095e9273d8a78d227d0a709
David G. Paul <jedi58@gmail.com>
2016-06-21 13:56:43 +0100

    [#2] Added support for numbered lists

--- src/WordpressToMarkdown.php
+++ src/WordpressToMarkdown.php
@@ -111,6 +111,16 @@ class WordpressToMarkdown
     {
         preg_match('/<div class="entry-content">([\w\W]*?)<!-- .entry-content -->/', self::$content, $match);
         if (!empty($match[0])) {
+            $match[0] = preg_replace_callback('/<ol>[\w\W]+?<\/ol>/', function ($m) 
+            {
+                $count = 1;
+                $list = preg_replace_callback('/<li>([\w\W]+?)<\/li>/', function ($m2) use (&$count) {
+                    return ($count++) . ' ' . $m2[1];
+                }, $m);
+                return $list[0];
+            }, $match[0]);
+
+
             self::$post['content'] = trim(strip_tags(preg_replace(
                 array(
                     '/<h1>/',
