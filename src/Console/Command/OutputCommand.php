68d68b59c3aa0a66ba7a88202434cab501c90be6
jedi58 <jedi58@gmail.com>
2016-06-21 06:44:05 -0400

    Applied fixes from StyleCI

--- src/Console/Command/OutputCommand.php
+++ src/Console/Command/OutputCommand.php
@@ -8,18 +8,17 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Formatter\OutputFormatterStyle;
-
 use Jedi58\WordpressToMarkdown\WordpressToMarkdown;
 
 class OutputCommand extends Command
 {
-	protected function configure()
-	{
-		$this
+    protected function configure()
+    {
+        $this
             ->setName('output')
             ->setDescription('Outputs one or more Wordpress posts to stdout')
             ->addArgument('url', InputArgument::REQUIRED, 'The URL to process Wordpress post(s) from');
-	}
+    }
     /**
      * Creates the Jira ticket and outputs the issue-key
      * @param InputInterface $input The console input object
@@ -29,5 +28,5 @@ class OutputCommand extends Command
     {
         $url = $input->getArgument('url');
         WordpressToMarkdown::output($url);
-	}
+    }
 }
